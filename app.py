bd73f063043d85081ee329e3a6ec6470cd2fe4d7
Lucy Park <me@lucypark.kr>
2014-10-14 16:34:04 +0900

    Inject menus using a context processor

--- app.py
+++ app.py
@@ -12,15 +12,15 @@ app.jinja_env.globals.update(isinstance=isinstance, str=str)
 
 @app.route('/')
 def home():
-    return render_template('home.html', menus=MENUS)
+    return render_template('home.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', menus=MENUS)
+    return render_template('contact.html')
 
 @app.route('/courses')
 def courses():
-    return render_template('courses.html', menus=MENUS,
+    return render_template('courses.html',
            courses=read_json_data('courses.json'))
 
 @app.route('/faq/')
@@ -30,19 +30,16 @@ def dummy_faq():
 @app.route('/faq/datamining')
 def datamining():
     return render_template('datamining.html',\
-           menus=MENUS,
            datamining=read_json_data('datamining.json'))
 
 @app.route('/faq/admission')
 def admission():
     return render_template('admission.html',\
-           menus=MENUS,
            admission=read_json_data('admission.json'))
 
 @app.route('/members')
 def members():
     return render_template('members.html',\
-           menus=MENUS,
            members=read_json_data('members.json'),
            alumni_phd=read_csv_data('alumni_phd.csv'),
            alumni_ms=read_csv_data('alumni_ms.tsv', sep='\t'))
@@ -54,13 +51,11 @@ def projects():
 @app.route('/projects/topics')
 def topics():
     return render_template('topics.html',\
-           menus=MENUS,
            topics=read_json_data('topics.json'))
 
 @app.route('/projects/faq')
 def faq():
     return render_template('faq.html',\
-           menus=MENUS,
            faq=read_json_data('faq.json'))
 
 @app.route('/research/')
@@ -69,13 +64,11 @@ def research():
 
 @app.route('/research/methods')
 def methods():
-    return render_template('methods.html',\
-           menus=MENUS)
+    return render_template('methods.html')
 
 @app.route('/research/publications')
 def publications():
     return render_template('publications.html',\
-           menus=MENUS,
            pub_dom_conferences=read_txt_data('pub_dom_conferences.txt'),
            pub_dom_journals=read_txt_data('pub_dom_journals.txt'),
            pub_int_conferences=read_txt_data('pub_int_conferences.txt'),
@@ -85,12 +78,15 @@ def publications():
 @app.route('/software')
 def software():
     return render_template('software.html',\
-           menus=MENUS,
            software=read_json_data('software.json'))
 
 @app.route('/~<name>')
 def member(name):
     return redirect('%s/~%s' % (APP_URL, name))
+
+@app.context_processor
+def inject_menus():
+    return dict(menus=MENUS)
 
 if __name__=='__main__':
     app.run(SERVER['host'], SERVER['port'])
