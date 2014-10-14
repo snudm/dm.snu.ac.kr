5eeb5c1a2b87f7418083c52e1496e5952691f2f0
Lucy Park <me@lucypark.kr>
2014-10-14 22:54:45 +0900

    Update i18n

--- settings.py
+++ settings.py
@@ -1,5 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import os
 
+
+CONTACT = {
+    'address_ko': '서울시 관악구 관악로 1 서울대학교 39동 339호',
+    'address_en': '39-339, Seoul National University, Gwanak-ro, Gwanak-gu, Seoul, South Korea (Zipcode: 151-742)',
+    'email': 'datamining@snu.ac.kr',
+    'phone': '+82-(0)2-880-7025',
+}
 
 MENUS = [
     'members',
