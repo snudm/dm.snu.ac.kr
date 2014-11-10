446ccbb448e85c74b5caa4483cccacb6c817571c
Hyun Chang Cho <eva0071@gmail.com>
2014-11-10 21:49:13 +0900

    menu change

---
+++ dm.snu.ac.kr/settings.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+CONTACT = {
+    'address_ko': '서울시 관악구 관악로 1 서울대학교 39동 339호',
+    'address_en': '39-339, Seoul National University, Gwanak-ro, Gwanak-gu, Seoul, South Korea (Zipcode: 151-742)',
+    'email': 'datamining@snu.ac.kr',
+    'phone': '+82-(0)2-880-7025',
+}
+
+MENUS = [
+    'datamining',
+    'members',
+    ('research', ['methods', 'publications']),
+    ('projects', ['topics', 'FAQ']),
+    'courses',
+    'seminar',
+    'software',
+    'admission',
+    'contact'
+]
+
+BABEL = {
+    'default_locale': 'ko',
+}
+
+SERVER = {
+    'host': '0.0.0.0',
+    'port': 8088,
+    'debug': True,
+}
+
+APP_URL = 'http://dm.snu.ac.kr'
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_STATIC = os.path.join(APP_ROOT, 'static')
---
+++ dm.snu.ac.kr/utils.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python2.7
+# -*- coding: utf-8 -*-
+
+import json
+import os
+
+from settings import APP_STATIC
+
+
+def read_csv_data(filename, sep=','):
+    filepath = os.path.join(APP_STATIC, 'data', filename)
+    with open(filepath, 'r') as f:
+        return [row.decode('utf-8').split(sep)\
+            for row in f.read().splitlines()]
+
+def read_json_data(filename):
+    filepath = os.path.join(APP_STATIC, 'data', filename)
+    with open(filepath, 'r') as f:
+        return json.load(f, encoding='utf-8')
+
+def read_txt_data(filename):
+    filepath = os.path.join(APP_STATIC, 'data', filename)
+    with open(filepath, 'r') as f:
+        return f.read().decode('utf-8').splitlines()
