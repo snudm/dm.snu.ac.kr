5367de5836b0941877e03fd71864771ad7ab4cfe
hoseong <dedert@naver.com>
2015-06-07 16:24:44 +0900

    Notice popup about summer internship

--- static/js/snudm-notice.js
+++ static/js/snudm-notice.js
@@ -1,7 +1,7 @@
 var n = noty({
     layout: 'topRight',
     theme: 'relax',
-    text: '데마센터 겨울방학 인턴을 모집합니다! <a href="/ko/notices/20141206" class="pull-right">[바로가기]</a>',
+    text: '[공지]2015 하계방학 인턴 관련 공고<a href="/ko/notices/20150607" class="pull-right">[바로가기]</a>',
     animation: {
         open: 'animated bounceInDown',
         close: {height: 'toggle'}, // jQuery animate function property object
