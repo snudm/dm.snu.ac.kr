df75a67bab64e360cf70af45ff59dba92d6e045b
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2018-12-03 14:45:07 +0900

    notice popup on 2018 yearend

--- static/js/snudm-notice.js
+++ static/js/snudm-notice.js
@@ -1,7 +1,7 @@
 var n = noty({
     layout: 'topRight',
     theme: 'relax',
-    text: '- 안진원, 류성원 NIPS 발표<br/>- 김은지, 최민 KDMS 최우수논문상 수상<br/><img class="width-49" src="/static/images/news/2018_kdms.jpg">- 강연국 AAAI 발표<br/>',
+    text: '<strong>[데이터마이닝 연구실 연말 연구활동 성과]</strong><br>- 안진원, 류성원 NIPS 발표<br>- 김은지, 최민 KDMS 최우수논문상 수상<br>- 강연국 AAAI 발표<br><img class="width-40" src="/static/images/news/2018_kdms.jpg">',
     animation: {
         open: 'animated bounceInDown',
         close: {height: 'toggle'}, // jQuery animate function property object
