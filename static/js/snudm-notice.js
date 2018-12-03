c8da45935de96d181d183287b0229bb8a6a9bb2a
sophia-jihye <jihyeparkk@dm.snu.ac.kr>
2018-12-03 14:05:05 +0900

    Add popup messages about kdms,nips,aaai

--- static/js/snudm-notice.js
+++ static/js/snudm-notice.js
@@ -1,7 +1,7 @@
 var n = noty({
     layout: 'topRight',
     theme: 'relax',
-    text: '[NEWS]김은지, 최민 KDMS 최우수논문상 수상<br/>><img class="width-49" src="/static/images/news/2018_kdms.jpg">',
+    text: '- 안진원, 류성원 NIPS 발표<br/>- 김은지, 최민 KDMS 최우수논문상 수상<br/><img class="width-49" src="/static/images/news/2018_kdms.jpg">- 강연국 AAAI 발표<br/>',
     animation: {
         open: 'animated bounceInDown',
         close: {height: 'toggle'}, // jQuery animate function property object
