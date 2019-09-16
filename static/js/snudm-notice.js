2ac8183cee0b44ec1ca6652e04dbe1ca804a0b8b
sylim2357 <sylim2357@gmail.com>
2019-09-16 16:30:22 +0900

    김은지박사 최우수논문상 수상 notification

--- static/js/snudm-notice.js
+++ static/js/snudm-notice.js
@@ -1,7 +1,20 @@
+/*
 var n = noty({
     layout: 'topRight',
     theme: 'relax',
     text: '<strong>[데이터마이닝 연구실 연말 연구활동 성과]</strong><br>- 안진원, 류성원 NIPS Workshop 발표<br>- 김은지, 최민 KDMS 최우수논문상 수상<br>- 강연국 AAAI 발표<br><a href="/ko/notices/20181203" class="pull-right">[바로가기]</a>',
+    animation: {
+        open: 'animated bounceInDown',
+        close: {height: 'toggle'}, // jQuery animate function property object
+        easing: 'swing', // easing
+        speed: 500 // opening & closing animation speed
+    }
+});
+*/
+var n = noty({
+    layout: 'topRight',
+    theme: 'relax',
+    text: '<strong>[서울대학교-삼성전자 2019 산학협력 교류회 성과]</strong><br><br>- 김은지 박사 (2019. 08 졸업), 최우수논문상 수상<br><br><a href="/ko/notices/20190916" class="pull-right">[바로가기]</a>',
     animation: {
         open: 'animated bounceInDown',
         close: {height: 'toggle'}, // jQuery animate function property object
