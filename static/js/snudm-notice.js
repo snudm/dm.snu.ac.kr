cca1bc3aba8751e0affd5dc44ff5ccbb506066be
JH Park <jihyeparkk@dm.snu.ac.kr>
2019-10-12 20:06:11 +0900

    create popup_exit a tag

--- static/js/snudm-notice.js
+++ static/js/snudm-notice.js
@@ -14,7 +14,7 @@ var n = noty({
 var n = noty({
     layout: 'topRight',
     theme: 'relax',
-    text: '<strong>[서울대학교-삼성전자 2019 산학협력 교류회 성과]</strong><br><br>- 김은지 박사 (2019. 08 졸업), 최우수논문상 수상<br><br><a href="/ko/notices/20190916" class="pull-right">[바로가기]</a>',
+    text: '<strong>[서울대학교-삼성전자 2019 산학협력 교류회 성과]</strong><br><br>- 김은지 박사 (2019. 08 졸업), 최우수논문상 수상<br><br><a onclick="document.getElementById('noty_topRight_layout_container').innerHTML=''">[팝업 닫기]</a><a href="/ko/notices/20190916" class="pull-right">[바로가기]</a>',
     animation: {
         open: 'animated bounceInDown',
         close: {height: 'toggle'}, // jQuery animate function property object
